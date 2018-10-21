91da3fd5417ce0d2cedd00db7cd93f5b7fac097c
Adruan <aroberes@gmail.com>
2018-10-21 10:42:38 -0700

    agaiin

--- index.js
+++ index.js
@@ -2,19 +2,7 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
-var http = require('http');
-var server = http.Server(app);
-
-app.use(express.static('client'));
-
-server.listen(PORT, function() {
-  console.log('Chat server running');
+app.get("/", function (req, res){
+	res.send("Welcome to NodeJS App on Heroku!");
 });
-
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
+app.listen(port);
