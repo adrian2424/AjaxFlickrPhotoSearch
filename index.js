c851d75a834db0937e15b5ebda789151f6d5a0e9
adrian2424 <38673737+adrian2424@users.noreply.github.com>
2018-10-30 16:54:46 -0700

    fix attempt to default

--- index.js
+++
@@ -1,8 +0,0 @@
-var PORT = process.env.PORT || 3000;
-var express = require('express');
-var app = express();
-
-app.get("/", function (req, res){
-	res.send("Welcome to NodeJS App on Heroku!");
-});
-app.listen(port);
